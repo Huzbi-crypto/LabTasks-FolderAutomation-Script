cf633cd75ea40496a3f97ae17ffa48ecdb88ffdb
Huzbi-crypto <huzbinaru@gmail.com>
2023-10-10 00:31:52 +0500

    Changes to be committed: 	modified:   createLabFiles.py
    📝 Added some more subtle functionalities to make it more flexible.

--- createLabFiles.py
+++ createLabFiles.py
@@ -1,7 +1,8 @@
 import os
 
 # Define the name of the directory to be created
-dir_name = "Lab-0"
+lab_no = input("Enter your lab number: ")
+dir_name = f'Lab-0{lab_no}'
 sub_dir_name = "source-codes"
 
 # Create target Directory
@@ -13,9 +14,12 @@ print("Directory ", dir_name, "/", sub_dir_name, " Created ")
 # Ask user for number of files to create
 num_files = int(input("Enter the number of .cpp files to create: "))
 
+# Ask user their student ID
+std_id = input("Enter your student ID in this format: ")
+
 # Create .cpp files
 for i in range(num_files):
-    file_name = f"q{i+1}.cpp"
+    file_name = f"q{i+1}_{std_id}.cpp"
     open(os.path.join(path, file_name), 'w').close()
     print(f"{file_name} created")
 
